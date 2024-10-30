d3bc9dfa2f73f0f5c77269489c1b19374a9afc4c
Johnathan Ulbert Ross <admin@stupidity.place>
2024-10-30 09:02:22 +0000

    Add "/jellyfin/login" route UI

--- lib/main.dart
+++ lib/main.dart
@@ -82,8 +82,63 @@ class login_state extends State<login> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Form(
+        key: key,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 62.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a Jellyfin Server URL";
+                    //add proper validation later
+                  }
+                  return null;
+                },
+                decoration:
+                    const InputDecoration(labelText: "Jellyfin Server URL"),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 62),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    validator: (value) {
+                      return null;
+                    },
+                    decoration: const InputDecoration(labelText: "Username"),
+                  )),
+                  Expanded(
+                      child: TextFormField(
+                    validator: (value) {
+                      return null;
+                    },
+                    decoration: const InputDecoration(labelText: "Password"),
+                    obscureText: true,
+                  )),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 62, vertical: 62),
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (key.currentState!.validate()) {
+                      router.go("/jellyfin");
+                    }
+                  },
+                  child: const Text("Login")),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
